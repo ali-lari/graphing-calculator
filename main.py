8870bd32a55f3b0bc6ae87d235805be364812b09
Ali Lari <alijasemlari@gmail.com>
2021-07-13 22:54:01 +0300

    make simple 2D cartesian plot

---
+++ main.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import math
+
+#TODO:
+#Create plot (Cartesian coordinates)
+#Create plot (r-theta coordinates)
+#Translate function to plot given domain
+plt.subplot() 
+plt.plot([1, 2, 3, 4], [1, 4, 2, 3])
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid()
+plt.show()
